f20d8934bb17cffedad47917deda0c456b24a8b1
sayantac <sccybernova111@gmail.com>
2024-02-16 10:45:35 +0000

    Cleanup

--- genai/semantic_kernel/summarizer.py
+++ genai/semantic_kernel/summarizer.py
@@ -4,18 +4,28 @@ from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
 kernel = sk.Kernel()
 
 api_key, org_id = sk.openai_settings_from_dot_env()
-kernel.add_text_completion_service("dv", OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id))
+openai_chat_service = OpenAIChatCompletion(ai_model_id="gpt-3.5-turbo", api_key=api_key, org_id=org_id)
 
-prompt = kernel.create_semantic_function("""1) A robot may not injure a human being or, through inaction, allow a human being to come to harm.
+kernel.add_text_completion_service("chat-gpt", openai_chat_service)
+
+sk_prompt = """{{$input}}
+
+Give me one line TLDR with the fewest words.
+"""
+
+prompt1 = """1) A robot may not injure a human being or, through inaction, allow a human being to come to harm.
 2) A robot must obey orders given it by human beings except where such orders would conflict with the First Law.
 3) A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.
-                                         
-Give me the TLDR in exactly 5 words.""")
+"""
 
-#print(prompt())
-
-summarize = kernel.create_semantic_function("{{$input}} \n\n one line TLDR with the fewest words")
-
-print(summarize("""1st Law of Thermodynamics - Energy cannot be created or destroyed.
+prompt2 = """1st Law of Thermodynamics - Energy cannot be created or destroyed.
 2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
-3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy."""))
+3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy."""
+
+# Run the prompt
+summarize = kernel.create_semantic_function(
+    prompt_template=sk_prompt, max_tokens=200, temperature=0, top_p=0.5)
+
+summary = await summarize(pr)
+
+print(f"Output: {summary}")  # Output: Robots must not harm humans.
