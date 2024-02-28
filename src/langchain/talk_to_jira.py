99aeb859a114c1199f2100fbaece0d4c4713ef90
sayantac <sccybernova111@gmail.com>
2024-02-28 11:07:50 +0000

    Jira

---
+++ src/langchain/talk_to_jira.py
@@ -0,0 +1,13 @@
+from langchain.agents import AgentType, initialize_agent
+from langchain_community.agent_toolkits.jira.toolkit import JiraToolkit
+from langchain_community.utilities.jira import JiraAPIWrapper
+from langchain_openai import OpenAI
+
+llm = OpenAI(temperature=0)
+jira = JiraAPIWrapper()
+toolkit = JiraToolkit.from_jira_api_wrapper(jira)
+agent = initialize_agent(
+    toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
+
+agent.run("Read the backlog from project 'ANA AI Initiative' and list out the stories")
